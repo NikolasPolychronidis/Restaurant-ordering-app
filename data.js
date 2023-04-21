783018e2512381d7e1511c4de6d75f4f8d3a81d7
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-04-21 17:34:59 +0200

    Add complete order and payment sections

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const menuArray = [
   {
     name: 'Pizza',
     ingredients: ['pepperoni', 'mushrom', 'mozarella'],
-    id: 0,
+    id: '0',
     price: 14,
     emoji: '🍕',
   },
@@ -11,13 +11,13 @@ export const menuArray = [
     ingredients: ['beef', 'cheese', 'lettuce'],
     price: 12,
     emoji: '🍔',
-    id: 1,
+    id: '1',
   },
   {
     name: 'Beer',
     ingredients: ['grain, hops, yeast, water'],
     price: 12,
     emoji: '🍺',
-    id: 2,
+    id: '2',
   },
 ];
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
 import { menuArray } from './data.js';
-const addToCardBtn = document.getElementById('add-to-cart-btn');
+const orderModal = document.getElementById('order-modal');
+const orderModalInner = document.getElementById('order-modal-inner');
+let itemsArray = [];
+const innerModal = document.getElementById('order-modal-inner');
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.buy) {
+    addItem(getItemObject(e.target.dataset.buy));
+  }
+});
+
 function getFeedHtml() {
   let feedHtml = '';
   menuArray.forEach(function (item) {
@@ -14,23 +23,29 @@ function getFeedHtml() {
             <p class='product-description'>${item.ingredients}</p>
             <p class='price'>$${item.price}</p>
         </div>
-            <button class='add-to-cart-btn' id='add-to-cart-btn' data-buy="${item.id}">+</button>
+            <button class='add-to-cart-btn' data-buy="${item.id}">+</button>
         <br>
     </div>
         `;
   });
   return feedHtml;
 }
+// Create function to get the selected item object from the menuArray
+function getItemObject(chosenItem) {
+  // Filter the objects in array to get the object that matched the item.id
+  const itemObject = menuArray.filter(function (item) {
+    return item.id === chosenItem;
+  });
+  return itemObject;
+}
+
+function addItem(itemObject) {
+  //add the item to items array
+  itemsArray.push(itemObject);
+}
+
 
 function render() {
   document.getElementById('feed').innerHTML = getFeedHtml();
 }
 render();
-
-// when add to cart btn is pressed, the order modal appears with the item you selected and a subtotal
-addToCardBtn.addEventListener('click', function (itemId) {
-  const targetItemObject = menuArray.filter(function (item) {
-    return (item.id = itemId);
-  })[0];
-  //push each item to a new array. If the iem exists, as the price to the existing total.
-});
