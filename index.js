7a73dc008a92aa811653d623f6d0a6a1b71c0cec
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-04-21 21:13:26 +0200

    Add add to cart functionality

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { menuArray } from './data.js';
 const orderModal = document.getElementById('order-modal');
 const orderModalInner = document.getElementById('order-modal-inner');
 let itemsArray = [];
-const innerModal = document.getElementById('order-modal-inner');
 document.addEventListener('click', function (e) {
   if (e.target.dataset.buy) {
     addItem(getItemObject(e.target.dataset.buy));
+    renderOrder();
   }
 });
 
@@ -35,7 +35,7 @@ function getItemObject(chosenItem) {
   // Filter the objects in array to get the object that matched the item.id
   const itemObject = menuArray.filter(function (item) {
     return item.id === chosenItem;
-  });
+  })[0];
   return itemObject;
 }
 
@@ -44,8 +44,33 @@ function addItem(itemObject) {
   itemsArray.push(itemObject);
 }
 
-
 function render() {
   document.getElementById('feed').innerHTML = getFeedHtml();
 }
+
+function renderOrder() {
+  let orderHtml = '';
+  let totalPrice = 0;
+  itemsArray.forEach(function (item) {
+    orderHtml += `
+    <div class="order-item-div" id="order-item-div">
+        <p class="item-name" id="item-name"> ${item.name}</p>
+        <p class="item-price" id="item-price"> $${item.price}</p>
+      </div>
+      <br />
+    `;
+    totalPrice += item.price;
+  });
+  orderHtml += `
+  <div class="total-price-div" id="total-price-div">
+      <p>Total price</p>
+      <p class="total-price" id="total-price">$${totalPrice}</p>
+    </div>
+    <button class="complete-order" id="complete-order">
+      Complete Order
+    </button>
+  `;
+  orderModalInner.innerHTML = orderHtml;
+}
+
 render();
