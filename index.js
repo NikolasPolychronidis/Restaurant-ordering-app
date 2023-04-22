8b1ff9704b08870452b093050df703fd514e5cb9
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-04-22 11:56:12 +0200

    Add remove item from cart functionality, payment form submission and thank you message

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 import { menuArray } from './data.js';
 const orderModal = document.getElementById('order-modal');
 const orderModalInner = document.getElementById('order-modal-inner');
+const checkoutForm = document.getElementById('checkout-form');
 let itemsArray = [];
 document.addEventListener('click', function (e) {
   if (e.target.dataset.buy) {
     addItem(getItemObject(e.target.dataset.buy));
     renderOrder();
+  } else if (e.target.dataset.remove) {
+    removeItem(e.target.dataset.remove);
+    renderOrder();
+  } else if (e.target.id === 'complete-order') {
+    completeOrderBtnClick();
   }
 });
 
@@ -44,24 +50,32 @@ function addItem(itemObject) {
   itemsArray.push(itemObject);
 }
 
+function removeItem(itemId) {
+  itemsArray = itemsArray.filter(function (item) {
+    return item.id !== itemId;
+  });
+}
+
 function render() {
   document.getElementById('feed').innerHTML = getFeedHtml();
 }
 
 function renderOrder() {
-  let orderHtml = '';
-  let totalPrice = 0;
-  itemsArray.forEach(function (item) {
-    orderHtml += `
+  if (itemsArray.length > 0) {
+    let orderHtml = '';
+    let totalPrice = 0;
+    itemsArray.forEach(function (item) {
+      orderHtml += `
     <div class="order-item-div" id="order-item-div">
         <p class="item-name" id="item-name"> ${item.name}</p>
+        <button class="remove-item" data-remove="${item.id}">Remove</button>
         <p class="item-price" id="item-price"> $${item.price}</p>
       </div>
       <br />
     `;
-    totalPrice += item.price;
-  });
-  orderHtml += `
+      totalPrice += item.price;
+    });
+    orderHtml += `
   <div class="total-price-div" id="total-price-div">
       <p>Total price</p>
       <p class="total-price" id="total-price">$${totalPrice}</p>
@@ -70,7 +84,37 @@ function renderOrder() {
       Complete Order
     </button>
   `;
-  orderModalInner.innerHTML = orderHtml;
+    orderModalInner.innerHTML = orderHtml;
+    orderModal.style.display = 'inline-block';
+  } else {
+    orderModal.style.display = 'none';
+  }
+}
+render();
+
+function completeOrderBtnClick() {
+  document
+    .getElementsByClassName('checkout-overlay')[0]
+    .classList.toggle('hidden');
+  document
+    .getElementsByClassName('checkout-modal')[0]
+    .classList.toggle('hidden');
 }
 
-render();
+checkoutForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const checkoutFormData = new FormData(checkoutForm);
+  const fullName = checkoutFormData.get('fullName');
+
+  setTimeout(function () {
+    document
+      .getElementsByClassName('checkout-overlay')[0]
+      .classList.toggle('hidden');
+    document
+      .getElementsByClassName('checkout-modal')[0]
+      .classList.toggle('hidden');
+    orderModalInner.innerHTML = `
+      <h2>Thanks <span class="modal-display-name">${fullName}</span>, your order will be delivered shortly! </h2>
+    `;
+  }, 500);
+});
